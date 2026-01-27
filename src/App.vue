45db96137664b5b5fe5041af65559f3fd313500d
sr <stepan.rutz@gmx.de>
2026-01-27 20:50:00 +0100

    drop

---
+++ src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="w-screen h-screen bg-zinc-800 text-white p-4 flex flex-col gap-2"></div>
+</template>
+
+<style>
+@import 'tailwindcss';
+</style>
